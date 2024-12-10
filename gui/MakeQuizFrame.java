84e6ed129fc721e767681023b09f61a6ebef4798
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2024-12-10 13:10:47 +0800

    update MakeQuizFrame gui

--- gui/MakeQuizFrame.java
+++ gui/MakeQuizFrame.java
@@ -1,73 +1,106 @@
 package gui;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.*;
 import utils.*;
 
 public class MakeQuizFrame extends JFrame {
     private QuizBuilder quizBuilder = new QuizBuilder("Untitled");
+    private QuizBuilder.PartialQuestionWithAnswer editing = new QuizBuilder.PartialQuestionWithAnswer();
+    private JTextArea questionArea = new JTextArea();
+    private JRadioButton[] correctAnswerButtons = new JRadioButton[4];
+    private JTextArea[] optionAreas = new JTextArea[4];
+
+    public static void main(String[] args) {
+        MakeQuizFrame f = new MakeQuizFrame();
+    }
 
     public MakeQuizFrame() {
         setTitle("Quiz Builder");
-        this.setSize(600, 600);
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setLayout(null);
+        setSize(600, 600);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new BorderLayout(10, 10));
 
-        JLabel questionLabel = new JLabel("Enter Question:");
-        questionLabel.setBounds(30, 30, 150, 30);
-        this.add(questionLabel);
+        JPanel questionPanel = new JPanel(new BorderLayout());
 
-        JTextField questionField = new JTextField();
-        questionField.setBounds(150, 30, 400, 30);
-        this.add(questionField);
+        JLabel questionLabel = new JLabel("Question", SwingConstants.CENTER);
+        questionPanel.add(questionLabel, BorderLayout.NORTH);
 
-        JLabel answerLabel = new JLabel("Answers:");
-        answerLabel.setBounds(30, 80, 150, 30);
-        this.add(answerLabel);
+        questionPanel.add(new JScrollPane(this.questionArea), BorderLayout.CENTER);
 
-        JTextField[] answerFields = new JTextField[4];
+        JPanel buttonPanel = new JPanel();
+        questionPanel.add(new JScrollPane(buttonPanel, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS), BorderLayout.SOUTH);
+
+        // for (int i = 0; i < 20; i++) {
+        //     JButton btn = new JButton(String.format("Q%d", i));
+        //     buttonPanel.add(btn);
+        // }
+
+        JPanel answerPanel = new JPanel(new BorderLayout());
+        JPanel optionAreaPanel = new JPanel();
+        optionAreaPanel.setLayout(new GridLayout(4, 1, 0, 10));
+        JScrollPane[] optionAreaScrollPane = new JScrollPane[4];
+
         for (int i = 0; i < 4; i++) {
-            answerFields[i] = new JTextField("Answer " + i);
-            answerFields[i].setBounds(150, 80 + i * 50, 300, 30);
-            this.add(answerFields[i]);
+            this.optionAreas[i] = new JTextArea();
+            this.optionAreas[i].setColumns(20);
+            optionAreaScrollPane[i] = new JScrollPane(this.optionAreas[i]);
+            optionAreaPanel.add(optionAreaScrollPane[i]);
         }
 
-        JLabel correctLabel = new JLabel("Correct Answer:");
-        correctLabel.setBounds(30, 300, 150, 30);
-        this.add(correctLabel);
-
-        JRadioButton[] correctAnswerButtons = new JRadioButton[4];
+        JPanel correctAnswerButtonPanel = new JPanel();
+        correctAnswerButtonPanel.setLayout(new GridLayout(4, 1));
         ButtonGroup group = new ButtonGroup();
         for (int i = 0; i < 4; i++) {
-            correctAnswerButtons[i] = new JRadioButton(String.valueOf(i + 1));
-            correctAnswerButtons[i].setBounds(460, 80 + i * 50, 50, 30);
-            group.add(correctAnswerButtons[i]);
-            this.add(correctAnswerButtons[i]);
+            this.correctAnswerButtons[i] = new JRadioButton(String.valueOf((char) (65 + i)));
+            this.correctAnswerButtons[i].addActionListener(new SelectAsAnswer(optionAreaScrollPane, optionAreaScrollPane[i]));
+            group.add(this.correctAnswerButtons[i]);
+            correctAnswerButtonPanel.add(this.correctAnswerButtons[i]);
         }
 
+        answerPanel.add(new JLabel("Options", SwingConstants.CENTER), BorderLayout.NORTH);
+        answerPanel.add(optionAreaPanel, BorderLayout.CENTER);
+        answerPanel.add(correctAnswerButtonPanel, BorderLayout.EAST);
+
+        JPanel actionButtonPanel = new JPanel();
+        JButton addButton = new JButton("Add");
+        JButton deleteButton = new JButton("Delete");
+        JButton copyButton = new JButton("Copy");
         JButton saveButton = new JButton("Save");
-        saveButton.setBounds(150, 400, 100, 30);
-        saveButton.addActionListener(e -> {
-            String question = questionField.getText();
-            if (question.isEmpty()) {
-                JOptionPane.showMessageDialog(this, "Please enter a question!", "Error", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            StringBuilder questionData = new StringBuilder(question + "\n");
-            for (int i = 0; i < 4; i++) {
-                questionData.append((correctAnswerButtons[i].isSelected() ? "*" : "") + answerFields[i].getText() + "\n");
-            }
-            JOptionPane.showMessageDialog(this, "Question saved successfully!");
-        });
-        this.add(saveButton);
-
         JButton uploadButton = new JButton("Upload");
-        uploadButton.setBounds(400, 400, 100, 30);
-        this.add(uploadButton);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
 
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
+        actionButtonPanel.add(addButton);
+        actionButtonPanel.add(deleteButton);
+        actionButtonPanel.add(copyButton);
+        actionButtonPanel.add(saveButton);
+        actionButtonPanel.add(uploadButton);
+
+        add(questionPanel, BorderLayout.CENTER);
+        add(answerPanel, BorderLayout.EAST);
+        add(actionButtonPanel, BorderLayout.SOUTH);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
+
+class SelectAsAnswer implements ActionListener {
+    private JComponent[] components;
+    private JComponent component;
+
+    public SelectAsAnswer(JComponent[] components, JComponent component) {
+        this.components = components;
+        this.component = component;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (JComponent component : this.components) {
+            component.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        }
+
+        this.component.setBorder(BorderFactory.createLineBorder(Color.RED));
     }
 }
