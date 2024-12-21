5f3181c4380d6b34a6aada9ba1e4c768c6c88baa
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2024-12-21 13:45:57 +0800

    update menu UI

--- gui/MainMenu.java
+++ gui/MainMenu.java
@@ -1,53 +1,99 @@
 package gui;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+
 import javax.swing.*;
 
 public class MainMenu {
     private JFrame menuFrame;
+    private Desktop desktop;
 
     public static void main(String[] args) {
         new MainMenu();
     }
 
     public MainMenu() {
+        desktop = Desktop.getDesktop();
         menuFrame = new JFrame("刷題趣！");
-        menuFrame.setSize(400, 300);
-        menuFrame.setLayout(null);
+        menuFrame.setPreferredSize(new Dimension(300, 430));
         menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        menuFrame.setResizable(false);
+        // menuFrame.setResizable(false);
         menuFrame.setIconImage(Resource.icon.getImage());
+        Box box = Box.createVerticalBox();
+        JLabel icon = new JLabel(new ImageIcon(Resource.icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
+        icon.setAlignmentX(0.5f);
+        box.add(icon);
+        box.add(Box.createVerticalGlue());
+        int fillerHeight = 20;
 
         // Single Player Mode Button
+        Dimension buttonSize = new Dimension(140, 30);
         JButton singleplayerButton = new JButton("Singleplayer");
-        singleplayerButton.setBounds(130, 50, 140, 30);
-        menuFrame.add(singleplayerButton);
+        singleplayerButton.setMaximumSize(buttonSize);
+        singleplayerButton.setPreferredSize(buttonSize);
+        singleplayerButton.setAlignmentX(0.5f);
+        box.add(singleplayerButton);
         singleplayerButton.addActionListener(e -> {
             SingleplayerClient.runQuizDialog(menuFrame);
         });
 
+        box.add(Box.createVerticalStrut(fillerHeight));
+
         // Multiplayer Mode Button
         JButton multiplayerButton = new JButton("Multiplayer");
-        multiplayerButton.setBounds(130, 100, 140, 30);
-        menuFrame.add(multiplayerButton);
+        multiplayerButton.setMaximumSize(buttonSize);
+        multiplayerButton.setPreferredSize(buttonSize);
+        multiplayerButton.setAlignmentX(0.5f);
+        box.add(multiplayerButton);
         multiplayerButton.addActionListener(e -> {
             new LoginDialog(menuFrame, new MultiplayerLoginHandler(menuFrame));
         });
 
+        box.add(Box.createVerticalStrut(fillerHeight));
+
         // Ask Question Button
         JButton askQuestionButton = new JButton("Create Quiz");
-        askQuestionButton.setBounds(130, 150, 140, 30);
-        menuFrame.add(askQuestionButton);
+        askQuestionButton.setMaximumSize(buttonSize);
+        askQuestionButton.setPreferredSize(buttonSize);
+        askQuestionButton.setAlignmentX(0.5f);
+        box.add(askQuestionButton);
         askQuestionButton.addActionListener(e -> {
-            MakeQuizFrame f = new MakeQuizFrame();
+            new MakeQuizFrame();
             menuFrame.dispose();
         });
 
+        box.add(Box.createVerticalStrut(fillerHeight));
+
         // Exit Button
         JButton exitButton = new JButton("Exit");
-        exitButton.setBounds(130, 200, 140, 30);
-        menuFrame.add(exitButton);
-        exitButton.addActionListener(e1 -> System.exit(0));
+        exitButton.setMaximumSize(buttonSize);
+        exitButton.setPreferredSize(buttonSize);
+        exitButton.setAlignmentX(0.5f);
+        box.add(exitButton);
+        exitButton.addActionListener(e -> System.exit(0));
+        box.add(Box.createVerticalStrut(20));
 
+        JPanel logoPanel = new JPanel();
+        JButton github = new JButton(new ImageIcon(Resource.github.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+        github.addActionListener(e -> {
+            try {
+                desktop.browse(new URI("https://github.com/CarrieForle/quiz"));
+            } catch (Exception ex) {
+                System.out.println("Failed to browse webpage");
+            }
+        });
+
+        github.setPreferredSize(new Dimension(32, 32));
+        logoPanel.add(github);
+        
+        logoPanel.setMaximumSize(new Dimension(100, 48));
+        box.add(logoPanel);
+
+        box.setBorder(BorderFactory.createEmptyBorder(10, 10, 20, 10));
+        menuFrame.add(box);
+        menuFrame.pack();
         menuFrame.setLocationRelativeTo(null);
         menuFrame.setVisible(true);
     }
--- gui/Resource.java
+++ gui/Resource.java
@@ -5,10 +5,12 @@ import javax.swing.ImageIcon;
 public class Resource {
     public static final ImageIcon podium;
     public static final ImageIcon icon;
+    public static final ImageIcon github;
 
     static {
         ImageIcon tmp = new ImageIcon(Resource.class.getResource("/assets/podium.png"));
         podium = new ImageIcon(tmp.getImage().getScaledInstance(250, 105, java.awt.Image.SCALE_SMOOTH));
         icon = new ImageIcon(Resource.class.getResource("/assets/icon-mid.png"));
+        github = new ImageIcon(Resource.class.getResource("/assets/github.png"));
     }
 }
