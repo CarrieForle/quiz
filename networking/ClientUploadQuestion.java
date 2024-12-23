0a83ed1ed4c271f7319df6a8061f218ad56f5603
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2024-12-23 20:41:16 +0800

    remove commented codes

--- networking/ClientUploadQuestion.java
+++ networking/ClientUploadQuestion.java
@@ -4,30 +4,6 @@ import java.io.*;
 import utils.QuestionSet;
 
 public class ClientUploadQuestion {
-    // private void showInitialDialog() {
-    //     String[] options = {"Create New Question Bank", "Select Local Question Bank"};
-    //     int choice = JOptionPane.showOptionDialog(
-    //             this,
-    //             "Choose an option to start:",
-    //             "Question Bank",
-    //             JOptionPane.DEFAULT_OPTION,
-    //             JOptionPane.INFORMATION_MESSAGE,
-    //             null,
-    //             options,
-    //             options[0]
-    //     );
-
-    //     if (choice == 0) {
-    //         questions.clear();
-    //         buttonPanel.removeAll();
-    //         updateButtons();
-    //     } else if (choice == 1) {
-    //         loadQuestionBank();
-    //     } else {
-    //         System.exit(0);
-    //     }
-    // }
-
     public static void main(String[] args) {
 
     }
